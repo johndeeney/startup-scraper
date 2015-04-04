2447908141be7b15a86b86225f0ca284d41a81a8
John Deeney <JohnCDeeney@gamil.com>
2015-04-03 20:40:30 -0400

    Refactored getJob() into multiple functions.

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+from urlparse import urljoin
 from pyquery import PyQuery as pq
 
 def findNewBase(url):
@@ -6,6 +7,45 @@ def findNewBase(url):
         return url[:(-1 * len(url.split('/')[-1]))].rstrip('/') + '/'
     else:
         return url.rstrip('/') + '/'
+
+def getNewLink(r, link):
+        if link[0] == '/':
+            baseUrl = r.url.split('/')[0] + '//' + r.url.split('/')[2]
+            if baseUrl == '/':
+                fullLink = baseUrl + link[1:]
+            else:
+                fullLink = baseUrl + '/' + link[1:]
+        elif 'http' in link:
+            fullLink = link
+        else:
+            fullLink = findNewBase(r.url) + link
+        return fullLink
+
+def isValidLink(link,origin):
+    if link is None or link == '':
+        return False
+    if '#' in link:
+        return False
+    if 'http' in link and origin not in link.split('/')[2]:
+        return False
+    if link == '/':
+        return False
+    if '.pdf' in link:
+        return False
+    if '.mp4' in link:
+        return False
+    if '.zip' in link:
+        return False
+    if 'mailto' in link:
+        return False
+    if 'javascript:' in link:
+        return False
+    if 'twitter.com' in link or 'facebook.com' in link or 'linkedin.com' in link:
+        return False
+    return True
+
+def containsInternship(r):
+    return 'intern' in r.content.lower() and r.content.lower().count('internal') + r.content.lower().count('international') + r.content.lower().count('internet') != r.content.lower().count('intern')
 
 def findJob(url,linklist,origin):
     global outputFile
@@ -24,42 +64,14 @@ def findJob(url,linklist,origin):
     except:
         return
     l = list(e('a').items())
-    if 'intern' in r.content.lower() and r.content.lower().count('internal') + r.content.lower().count('international') + r.content.lower().count('internet') != r.content.lower().count('intern'):
+    if containsInternship(r):
         print "Intern at: " + r.url
         outputFile.write(r.url + "\n")
     for attribute in l:
         link = attribute.attr['href']
-        if link is None or link == '':
+        if not isValidLink(link, origin):
             continue
-        if '#' in link:
-            continue
-        if 'http' in link and origin not in link.split('/')[2]:
-            continue
-        if link == '/':
-            continue
-        if '.pdf' in link:
-            continue
-        if '.mp4' in link:
-            pass
-        if '.zip' in link:
-            pass
-        if 'mailto' in link:
-            continue
-        if 'javascript:' in link:
-            continue
-        if 'twitter.com' in link or 'facebook.com' in link or 'linkedin.com' in link:
-            continue
-        if link[0] == '/':
-            baseUrl = r.url.split('/')[0] + '//' + r.url.split('/')[2]
-            if baseUrl == '/':
-                fullLink = baseUrl + link[1:]
-            else:
-                fullLink = baseUrl + '/' + link[1:]
-        elif 'http' in link:
-            fullLink = link
-        else:
-            fullLink = findNewBase(r.url) + link
-        
+        fullLink = getNewLink(r, link)
         if link not in linklist and 'facebook.com' not in fullLink and 'twitter.com' not in fullLink and 'linkedin.com' not in fullLink:
             if supercounter > 30:
                 print 'too many tries'
@@ -69,23 +81,26 @@ def findJob(url,linklist,origin):
             findJob(fullLink,linklist,origin)
 
 inputFile = open('E:/output2.txt','r')
-outputFile = open('E:/jobs2.txt', 'w')
+outputFile = open('E:/jobs4.txt', 'w')
 counter = 0
 supercounter = 0
 for line in inputFile:
-    if counter < 1078:
+    if counter < 1201:
         counter = counter + 1
         continue
     print "Opening " + line
+    original = line
     try:
-        parts = line.split('/')[-2].split('.')
-        original = parts[-2] + '.' + parts[-1]
+        #parts = line.split('/')[-2].split('.')
+        #original = parts[-2] + '.' + parts[-1]
+        original = line
     except:
         print 'Failed to parse ' + line
     try:
         supercounter = 0
         findJob(line,[],original)
-    except:
+    except Exception as inst:
         print 'Error for some reason at ' + line
+        print inst
 inputFile.close();
 outputFile.close();
